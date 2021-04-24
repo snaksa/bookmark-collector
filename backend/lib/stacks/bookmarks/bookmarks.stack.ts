117038445fa9dfac2838cf86e84c11304081dbbe
Sinan Shaban <sinan.zikri@gmail.com>
2021-04-24 17:18:05 +0300

    WIP: add CORS

--- backend/lib/stacks/bookmarks/bookmarks.stack.ts
+++ backend/lib/stacks/bookmarks/bookmarks.stack.ts
@@ -1,56 +1,70 @@
-import { StackProps, Construct } from '@aws-cdk/core';
-import { LambdaIntegration } from '@aws-cdk/aws-apigateway';
-import { AwsResources } from '../../shared/enums/aws-resources';
-import { ApiGatewayRequestMethods } from '../../shared/enums/api-gateway-request-methods';
-import { CreateLambda } from './requests/create/create-lambda';
-import { DeleteLambda } from './requests/delete/delete-lambda';
-import { ListLambda } from './requests/list/list-lambda';
-import { BaseStack } from '../base.stack';
+import {StackProps, Construct} from '@aws-cdk/core';
+import {Cors, LambdaIntegration} from '@aws-cdk/aws-apigateway';
+import {AwsResources} from '../../shared/enums/aws-resources';
+import {ApiGatewayRequestMethods} from '../../shared/enums/api-gateway-request-methods';
+import {CreateLambda} from './requests/create/create-lambda';
+import {DeleteLambda} from './requests/delete/delete-lambda';
+import {ListLambda} from './requests/list/list-lambda';
+import {BaseStack} from '../base.stack';
 
 export class BookmarksStack extends BaseStack {
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-        this.loadTables();
-        this.loadApi();
-        this.loadAuth();
-        this.loadAuthorizer();
+    this.loadTables();
+    this.loadApi();
+    this.loadAuth();
+    this.loadAuthorizer();
 
-        const bookmarks = this.api.root.addResource('bookmarks');
+    const bookmarks = this.api.root.addResource('bookmarks', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: ['*'],
+        disableCache: true,
+      }
+    });
 
-        bookmarks.addMethod(
-            ApiGatewayRequestMethods.POST,
-            new LambdaIntegration(
-                new CreateLambda(this, 'create-lambda', {
-                    dbStore: this.dbStore,
-                })
-            ),
-            this.getAuthorization()
-        );
+    bookmarks.addMethod(
+      ApiGatewayRequestMethods.POST,
+      new LambdaIntegration(
+        new CreateLambda(this, 'create-lambda', {
+          dbStore: this.dbStore,
+        })
+      ),
+      this.getAuthorization()
+    );
 
-        bookmarks.addMethod(
-            ApiGatewayRequestMethods.GET,
-            new LambdaIntegration(
-                new ListLambda(this, 'list-lambda', {
-                    dbStore: this.dbStore,
-                    dbStoreGSI1: AwsResources.DB_STORE_TABLE_GSI1,
-                })
-            ),
-            this.getAuthorization()
-        );
+    bookmarks.addMethod(
+      ApiGatewayRequestMethods.GET,
+      new LambdaIntegration(
+        new ListLambda(this, 'list-lambda', {
+          dbStore: this.dbStore,
+          dbStoreGSI1: AwsResources.DB_STORE_TABLE_GSI1,
+        })
+      ),
+      this.getAuthorization()
+    );
 
-        const singleBookmark = bookmarks.addResource('{id}');
+    const singleBookmark = bookmarks.addResource('{id}', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: Cors.ALL_ORIGINS,
+        allowMethods: Cors.ALL_METHODS,
+        allowHeaders: ['*'],
+        disableCache: true,
+      }
+    });
 
-        singleBookmark.addMethod(
-            ApiGatewayRequestMethods.DELETE,
-            new LambdaIntegration(
-                new DeleteLambda(this, 'delete-lambda', {
-                    dbStore: this.dbStore,
-                    reversedDbStore: AwsResources.DB_STORE_TABLE_REVERSED
-                })
-            ),
-            this.getAuthorization()
-        );
-    }
+    singleBookmark.addMethod(
+      ApiGatewayRequestMethods.DELETE,
+      new LambdaIntegration(
+        new DeleteLambda(this, 'delete-lambda', {
+          dbStore: this.dbStore,
+          reversedDbStore: AwsResources.DB_STORE_TABLE_REVERSED
+        })
+      ),
+      this.getAuthorization()
+    );
+  }
 }
