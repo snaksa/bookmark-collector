34687ae4ebd88f0b1dfbd55079e4520c0a09cfb0
Sinan Shaban <sinan.zikri@gmail.com>
2021-05-14 22:04:41 +0300

    Fix nullable input parameter check

--- backend/lib/shared/validators/validator.ts
+++ backend/lib/shared/validators/validator.ts
@@ -1,13 +1,13 @@
 export class Validator {
-    public static notEmpty(input?: string): boolean {
-        return Boolean(input && input.length);
-    }
+  public static notEmpty(input?: string): boolean {
+    return Boolean(input && input.length);
+  }
 
-    public static notEmptyStringArray(input?: string[]): boolean {
-        return Boolean(input && input.length);
-    }
+  public static notEmptyStringArray(input?: string[]): boolean {
+    return Boolean(input && input.length);
+  }
 
-    public static notNull(input?: any): boolean {
-        return input === null;
-    }
+  public static notNull(input?: any): boolean {
+    return input !== null;
+  }
 }
