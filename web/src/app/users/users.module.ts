864c53d22c7b62c6e9fa181483d03f879c75b74c
Sinan Shaban <sinan.zikri@gmail.com>
2021-06-13 21:59:30 +0300

    Add users and home modules

---
+++ web/src/app/users/users.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+
+const routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
+})
+export class UsersModule {}
