98139a5fa4d9563552565ee412f1bec5a18ad6fe
Sinan Shaban <sinan.zikri@gmail.com>
2021-07-11 10:39:32 +0300

    Style new bookmark field

--- web/src/app/modules/authenticated/bookmarks/components/bookmarks-layout/bookmarks-layout.component.ts
+++ web/src/app/modules/authenticated/bookmarks/components/bookmarks-layout/bookmarks-layout.component.ts
@@ -1,24 +1,36 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {State} from "../../state/bookmarks.state";
-import {Store} from "@ngrx/store";
-import {createBookmarkAction} from "../../state/bookmarks.actions";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { State } from '../../state/bookmarks.state';
+import { Store } from '@ngrx/store';
+import { createBookmarkAction } from '../../state/bookmarks.actions';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bookmarks-layout',
   templateUrl: './bookmarks-layout.component.html',
   styleUrls: ['./bookmarks-layout.component.scss'],
 })
-export class BookmarksLayoutComponent {
+export class BookmarksLayoutComponent implements OnInit, OnDestroy {
+  changes: Subscription = new Subscription();
+  disabled = true;
   newBookmarkFormGroup: FormGroup = new FormGroup({
-    bookmark: new FormControl('', Validators.required),
+    bookmark: new FormControl(''),
   });
 
-  constructor(private store: Store<State>) {
+  constructor(private store: Store<State>) {}
+
+  ngOnInit() {
+    this.changes = this.newBookmarkFormGroup.controls['bookmark'].valueChanges.subscribe((value) => {
+      this.disabled = !value;
+    });
+  }
+
+  ngOnDestroy() {
+    this.changes.unsubscribe();
   }
 
   public onSubmit() {
     const url = this.newBookmarkFormGroup.controls['bookmark'].value;
-    this.store.dispatch(createBookmarkAction({url}));
+    this.store.dispatch(createBookmarkAction({ url }));
   }
 }
