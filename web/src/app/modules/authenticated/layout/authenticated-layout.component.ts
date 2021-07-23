e5cc53320a1caa549b36d72cbd5426aff8187221
Sinan Shaban <sinan.shaban@gtmhub.com>
2021-07-23 14:58:56 +0300

    Responsive sidebar

--- web/src/app/modules/authenticated/layout/authenticated-layout.component.ts
+++ web/src/app/modules/authenticated/layout/authenticated-layout.component.ts
@@ -1,9 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 import { User } from '../../shared/models/user.model';
 import { loadUserAction } from '../../../state/app.actions';
 import { getCurrentUserSelector } from '../../../state/app.selectors';
 import { AppState } from '../../../state/app.state';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-authenticated-layout',
@@ -11,7 +16,7 @@ import { AppState } from '../../../state/app.state';
   styleUrls: ['./authenticated-layout.component.scss'],
 })
 export class AuthenticatedLayoutComponent implements OnInit {
-  constructor(private store: Store<AppState>) {}
+  constructor(private router: Router, private breakpointObserver: BreakpointObserver, private store: Store<AppState>) {}
   currentUser: User = {
     id: '',
     firstName: '',
@@ -19,7 +24,13 @@ export class AuthenticatedLayoutComponent implements OnInit {
     email: '',
   };
 
+  @ViewChild('sidenav')
+  sidenav!: MatSidenav;
+
+  isHandset = false;
+
   ngOnInit() {
+    // get the current user
     this.store.select(getCurrentUserSelector).subscribe((user) => {
       if (!user.isInitialized && !user.isLoading) {
         this.store.dispatch(loadUserAction());
@@ -27,5 +38,25 @@ export class AuthenticatedLayoutComponent implements OnInit {
 
       this.currentUser = user.data;
     });
+
+    // watch for breakpoint changes
+    this.breakpointObserver.observe(Breakpoints.Handset)
+      .pipe(
+        map(result => result.matches),
+        shareReplay()
+      ).subscribe((handset) => {
+        this.isHandset = handset;
+    })
+
+    // hide sidebar on routing if on handset device
+    this.router.events.subscribe(event => {
+      if(this.isHandset) {
+        this.sidenav.close();
+      }
+    });
+  }
+
+  openSidenav() {
+    this.sidenav.open();
   }
 }
