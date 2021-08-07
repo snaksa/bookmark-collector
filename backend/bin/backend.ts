128caeb9f4534e93d678407995c20ca484d1dae0
Sinan Shaban <sinan.zikri@gmail.com>
2021-08-07 14:26:24 +0300

    Add env account details

--- backend/bin/backend.ts
+++ backend/bin/backend.ts
@@ -9,27 +9,32 @@ import { LabelsStack } from "../lib/stacks/labels/labels.stack";
 import { BookmarksStack } from "../lib/stacks/bookmarks/bookmarks.stack";
 import { FrontendStack } from "../lib/stacks/frontend/frontend.stack";
 
+const env = {
+  account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
+};
+
 const app = new cdk.App();
 
-const dbStack = new DbStack(app, "DbStack");
+const dbStack = new DbStack(app, "DbStack", { env });
 
-const cognitoStack = new CognitoStack(app, "CognitoStack");
+const cognitoStack = new CognitoStack(app, "CognitoStack", { env });
 cognitoStack.addDependency(dbStack);
 
-const apiGatewayStack = new ApiGatewayStack(app, "ApiGatewayStack");
+const apiGatewayStack = new ApiGatewayStack(app, "ApiGatewayStack", { env });
 apiGatewayStack.addDependency(cognitoStack);
 
-const usersStack = new UsersStack(app, "UsersStack");
+const usersStack = new UsersStack(app, "UsersStack", { env });
 usersStack.addDependency(dbStack);
 usersStack.addDependency(apiGatewayStack);
 usersStack.addDependency(cognitoStack);
 
-const labelsStack = new LabelsStack(app, "LabelsStack");
+const labelsStack = new LabelsStack(app, "LabelsStack", { env });
 labelsStack.addDependency(dbStack);
 labelsStack.addDependency(apiGatewayStack);
 
-const bookmarksStack = new BookmarksStack(app, "BookmarksStack");
+const bookmarksStack = new BookmarksStack(app, "BookmarksStack", { env });
 bookmarksStack.addDependency(dbStack);
 bookmarksStack.addDependency(apiGatewayStack);
 
-new FrontendStack(app, "FrontendStack");
+new FrontendStack(app, "FrontendStack", { env });
